3278e26ba9b6ba8596fd29e34a85a705c1331599
Joe <pyaephyokyaw.work@gmail.com>
2024-07-04 21:41:11 +0700

    feat: throttle with ip

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AddressModule } from './address/address.module';
 import { MongooseDbModule } from './mongoose-db/mongoose-db.module';
 import { MongooseModelModule } from './mongoose-model/mongoose-model.module';
-import { ConfigModule } from '@nestjs/config';
-import { APP_FILTER } from '@nestjs/core';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_FILTER, APP_GUARD } from '@nestjs/core';
 import { AllExceptionsFilter } from 'lib/common/filters/all-exceptions-filter';
 import { CsvModule } from './csv/csv.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { ONE_MINUTE, RateLimits } from './constant';
 
 @Module({
   imports: [
@@ -16,8 +18,26 @@ import { CsvModule } from './csv/csv.module';
     }),
     MongooseDbModule,
     MongooseModelModule,
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return [
+          {
+            ttl: config.get('ALL_TTL_MIN', RateLimits.all.ttl) * ONE_MINUTE,
+            limit: config.get('ALL_RATE_LIMIT', RateLimits.all.limit),
+          },
+        ];
+      },
+    }),
     CsvModule,
   ],
-  providers: [{ provide: APP_FILTER, useClass: AllExceptionsFilter }],
+  providers: [
+    { provide: APP_FILTER, useClass: AllExceptionsFilter },
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/constant.ts
+++ src/constant.ts
@@ -2,3 +2,11 @@ export const ADDRESS_COLLECTION_NAME = 'addresses';
 export const MAX_LENGTH_ADDRESS = Number(process.env.MAX_LENGTH_ADDRESS) || 100;
 export const MIN_LENGTH_ADDRESS = Number(process.env.MIN_LENGTH_ADDRESS) || 40;
 export const CSV_FIELDS = ['name', 'createdAt', 'userIp'];
+export const RateLimits = {
+  all: {
+    limit: 10,
+    ttl: 1, // in minutes
+  },
+};
+
+export const ONE_MINUTE = 60 * 1000;
